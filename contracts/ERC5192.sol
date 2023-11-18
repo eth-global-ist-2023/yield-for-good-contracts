f862ee9c46574c08d7b3abce58f17092c8f28a23
Stan Trenev <stan@0xfusion.com>
2023-11-18 12:12:13 +0300

    feat: implement ERC5192 Minimal Soulbound

---
+++ contracts/ERC5192.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC5192} from "./interfaces/IERC5192.sol";
+
+abstract contract ERC5192 is ERC721, IERC5192 {
+    bool private _locked;
+
+    error TransferLocked();
+
+    constructor(string memory name, string memory symbol, bool isLocked) ERC721(name, symbol) {
+        _locked = isLocked;
+    }
+
+    function locked(uint256 tokenId) external view virtual returns (bool) {
+        _requireOwned(tokenId);
+        return _locked;
+    }
+
+    function _checkLockStatus() private view {
+        if (_locked) revert TransferLocked();
+    }
+
+    function approve(address to, uint256 tokenId) public virtual override {
+        _checkLockStatus();
+        super.approve(to, tokenId);
+    }
+
+    function setApprovalForAll(address operator, bool approved) public virtual override {
+        _checkLockStatus();
+        super.setApprovalForAll(operator, approved);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
+        _checkLockStatus();
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public virtual override {
+        _checkLockStatus();
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+}
